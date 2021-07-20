b40812624544486eb3d60bab1ce0345461e9f32e
Masimthembe <mndabeni6@gmail.com>
2021-07-20 11:37:06 +0200

    updated changes

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import requests
 # initializing the application
 app = Flask(__name__)
 
-# creating a route
+# initial route
 @app.route('/', methods=['GET'])
 def get_chuck_norris_jokes():
-
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
-
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+    return  response
 
 if __name__ == '__main__':
     app.run(debug=True)
